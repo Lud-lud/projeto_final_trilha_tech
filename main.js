e0915e2038c13b37ee349be90310065291d20983
Lud-lud <ludmila.silva.resende@gmail.com>
2023-05-26 10:33:50 -0300

    correção de bugs

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function menuShow() {
-   let menuMobile = document.querySelector('.menu_mobile');
+   let menuMobile = document.querySelector('.menu-mobile');
    if (menuMobile.classList.contains('open')) {
     menuMobile.classList.remove('open');
     document.querySelector('.icon').src = "./assets/icons8-cardápio.svg"
