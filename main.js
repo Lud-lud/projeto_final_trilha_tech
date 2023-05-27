b68fef05c329f9e3f7495676ec2766c481c50528
Lud-lud <ludmila.silva.resende@gmail.com>
2023-05-27 19:06:32 -0300

    impelementação do menu mobile

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-function menuShow() {
-   let menuMobile = document.querySelector('.menu-mobile');
-   if (menuMobile.classList.contains('open')) {
-    menuMobile.classList.remove('open');
-    document.querySelector('.icon').src = "./assets/icons8-cardápio.svg"
-        } else {
-    menuMobile.classList.add('open');
-    document.querySelector('.icon').src = "./assets/icons8-excluir.svg";
-        } 
-        
+const btnMobile = document.getElementById('btn-mobile');
+const nav = document.getElementById('nav');
+let hamburguer = document.querySelector('.icon');
+
+function toggleMenu() {
+    nav.classList.toggle('active');
+    menuShow();
 }
 
+btnMobile.addEventListener('click', toggleMenu);
+
+function menuShow() {
+    
+    if (nav.classList.contains('active')) {
+        hamburguer.src = "./assets/icons8-excluir.svg";
+    } else {
+        hamburguer.src = "./assets/icons8-cardápio.svg";
+    }
+}
